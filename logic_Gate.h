84f3817aaf4a4a5f20ea8d90348b1d0f32b51667
XikaiXu <xuxikai12300@gmail.com>
2026-04-07 15:41:12 -0400

    rebuild the structure

--- logic_Gate.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-/*This is the gate we will have to use to build by the 2n3904 ttl circuit
- */
-void nor_gate(bool &y, bool &a, bool &b);
-void nor_gate(bool &y, bool &a, bool &b){
-    *y = ~(*a || *b);
-}
